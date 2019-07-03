049f5ff524323d8d99732322da77204a8ed846b6
Felipe <philipe.s.xavier@gmail.com>
2019-07-03 16:58:49 -0700

    eliminated a log

--- client/src/components/__tests__/ConsulateImg.test.jsx
+++ client/src/components/__tests__/ConsulateImg.test.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { render, shallow, mount, configure} from 'enzyme'
-import ConsulateImg from '../ConsulateImg.jsx';
-import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
+import React from "react";
+import { render, shallow, mount, configure } from "enzyme";
+import ConsulateImg from "../ConsulateImg.jsx";
+import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 
 configure({ adapter: new Adapter() });
 
-describe('ConsulateImg.jsx', () => {
-    it('Should render App without errors', () => {
-        render(<ConsulateImg  />)
-    })
+describe("ConsulateImg.jsx", () => {
+  it("Should render App without errors", () => {
+    render(<ConsulateImg />);
+  });
 
-    it('should render without throwing an error', () => {
-      var wrapper = shallow(<ConsulateImg src='test_src' />)
-      console.log('this is wrapper................', wrapper);
-      expect(wrapper.find('#consulateImg')).toBeDefined()
-      expect(wrapper.find())
-     })
-
-})
+  it("should render without throwing an error", () => {
+    var wrapper = shallow(<ConsulateImg src="test_src" />);
+    //   console.log('this is wrapper................', wrapper);
+    expect(wrapper.find("#consulateImg")).toBeDefined();
+    expect(wrapper.find());
+  });
+});
