56e4726a327b15535ae03ea497388def238e8fe5
Felipe Lima <fsxlima@gmail.com>
2021-03-20 20:36:19 -0400

    refactors routes to make index.js cleaner

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const resolvers = require("./resolvers");
-
 const router = express.Router();
 
 router.get("/animals", (req, res) => {
   res.json(["john", "tom"]);
 });
-
 router.get("/findPost/:name", resolvers.findName);
 router.get("/posts", resolvers.getAllPosts);
+router.get("/review/:name/:type", resolvers.getReviews);
+router.get("/boss/:name/:type", resolvers.getBossInfo);
+router.post("/review", resolvers.postReview);
 
 module.exports = router;
--- server/routes/resolvers.js
+++ server/routes/resolvers.js
@@ -1,5 +1,7 @@
 let findWithRegex = require("../../database-mongo/index").findWithRegex;
 let selectAll = require("../../database-mongo/index.js").selectAll;
+let findBoss = require("../../database-mongo/index.js").findBoss;
+let insertReview = require("../../database-mongo/index.js").insertReview;
 
 const findName = (req, res) => {
   let post = req.params.name;
@@ -26,4 +28,61 @@ const getAllPosts = (req, res) => {
     });
 };
 
-module.exports = { findName, getAllPosts };
+const getReviews = (req, res) => {
+  let city = req.params.name;
+  let type = req.params.type;
+
+  findReviews(city, type)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log("there was an error in the server", err);
+      res.status.send(404);
+    });
+};
+
+const getBossInfo = (req, res) => {
+  let city = req.params.name;
+  let type = req.params.type;
+  findBoss(city, type)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log("there was an error in the server", err);
+      res.status.send(404);
+    });
+};
+
+const postReview = (req, res) => {
+  let {
+    textAreaContent,
+    cost,
+    dates,
+    workPlaceRating,
+    fun,
+    safety,
+    type,
+    postName,
+  } = req.body;
+
+  insertReview(
+    textAreaContent,
+    dates,
+    cost,
+    workPlaceRating,
+    fun,
+    safety,
+    type,
+    postName
+  )
+    .then((result) => {
+      res.end();
+    })
+    .catch((err) => {
+      console.log("there was an error", err);
+    });
+};
+
+module.exports = { findName, getAllPosts, getReviews, getBossInfo, postReview };
